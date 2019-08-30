5273be8da8c73ce3d2d5dfa267560d56213f21c5
Michael Bloch <mchblch@gmail.com>
2019-08-30 22:24:10 +0200

    update

--- Bank Account Manager/main.cpp
+++ Bank Account Manager/main.cpp
@@ -2,36 +2,72 @@
 #include <string_view>
 #include <map>
 #include <optional>
+#include <deque>
+#include <string>
 
 template<typename T>
-struct IAccount
+struct crtp
 {
-	auto& deposit(float v) noexcept { static_cast<T*>(this)->deposit(v); return *this; }
-	auto& takeOut(float v) noexcept { static_cast<T*>(this)->takeOut(v); return *this; }
-	auto balance() const noexcept { return static_cast<const T*>(this)->balance(); }
+	constexpr crtp() = default;
+	constexpr auto* underlying() { return static_cast<T*>(this); }
+	constexpr const auto* underlying() const { return static_cast<const T*>(this); }
+};
+
+template<typename T>
+struct IAccountLog : public crtp<T>
+{
+	IAccountLog& logDeposit(float v) noexcept { this->underlying()->logDeposit(v); return *this; }
+	IAccountLog& logTakeOut(float v) noexcept { this->underlying()->logTakeOut(v); return *this; }
+
+protected:
+	IAccountLog() = default;
+};
+
+template<typename T>
+struct IAccount : public crtp<T>
+{
+	IAccount& deposit(float v) noexcept { this->underlying()->deposit(v); return *this; }
+	IAccount& takeOut(float v) noexcept { this->underlying()->takeOut(v); return *this; }
+	float balance() const noexcept { return this->underlying()->balance(); }
 
 protected:
 	IAccount() = default;
 };
 
-class Account : public IAccount<Account>
+template<typename ImplLog>
+class Account : public IAccount<Account<ImplLog>>
 {
-	auto& deposit(float v) noexcept { m_mon += v; return *this; }
-	auto& takeOut(float v) noexcept { m_mon -= v; return *this; }
-	auto balance() const { return m_mon; }
+	void deposit(float v) noexcept 
+	{ 
+		m_mon += v;
+		m_logger.first.get().logDeposit(v);
+	}
+	void takeOut(float v) noexcept 
+	{ 
+		m_mon -= v; 
+		m_logger.first.get().logTakeOut(v);
+	}
+	auto balance() const noexcept 
+	{
+		return m_mon; 
+	}
 
 public:
 	Account() = default;
+
 	friend struct IAccount<Account>;
 
 private:
-	float m_mon = 10.f;
+	float m_mon = 0.f;
+
+	std::pair<std::reference_wrapper<IAccountLog<ImplLog>>, ImplLog> m_logger 
+		= std::pair<std::reference_wrapper<IAccountLog<ImplLog>>, ImplLog>(m_logger.second, ImplLog());
 };
 
 template<typename T>
-struct IAccountDB
+struct IAccountDB : public crtp<T>
 {
-	auto& account(const std::string_view& str) { return static_cast<T*>(this)->account(str); }
+	auto& account(const std::string_view& str) { return this->underlying()->account(str); }
 
 protected:
 	IAccountDB() = default;
@@ -55,13 +91,12 @@ private:
 	std::map<std::string_view, T> m_accounts;
 };
 
-template</*typename ImplTrans, typename ImplLog, */typename ImplAccDB>
+template<typename ImplAccDB>
 class ATM
 {
 public:
 	ATM(IAccountDB<ImplAccDB>* db)
 		: m_db(db)
-		//, m_trans(db)
 	{
 	}
 
@@ -72,43 +107,34 @@ public:
 
 private:
 	IAccountDB<ImplAccDB>* m_db;
-	//ImplTrans m_trans;
-	//ImplLog m_log;
 };
 
-//template<typename IAccountDB>
-//class ATMTransactions
-//{
-//public:
-//	ATMTransactions(IAccountDB* db)
-//		: m_db(db)
-//	{
-//	}
-//
-//private:
-//	IAccountDB* m_db;
-//};
-//
-//template<typename IAccountDB>
-//class ATMLogger
-//{
-//public:
-//	ATMTransactions(IAccountDB* db)
-//		: m_db(db)
-//	{
-//	}
-//
-//private:
-//	IAccountDB* m_db;
-//};
+class AccountLogger : public IAccountLog<AccountLogger>
+{
+	void logDeposit(float v) noexcept 
+	{ 
+		m_logs.emplace_back(std::to_string(v) + " deposited");
+	}
+	void logTakeOut(float v) noexcept 
+	{ 
+		m_logs.emplace_back(std::to_string(v) + " took out");
+	}
+
+public:
+	AccountLogger() {}
+	friend struct IAccountLog<AccountLogger>;
+
+private:
+	std::deque<std::string> m_logs;
+};
 
 int main(int argc, char** argv)
 {
-	AccountDB<Account> db;
+	AccountDB<Account<AccountLogger>> db;
 	db.createAccount("ass");
 
 	ATM atm(&db);
-	std::cout << atm.access("ass").balance();
+	std::cout << atm.access("ass").deposit(10.f).balance();
 
 	return 0;
 }
