42faf3242383e65333751dd624309b44805e17e3
Michael Bloch <mchblch@gmail.com>
2019-08-31 10:11:20 +0200

    Pretty good

--- Bank Account Manager/main.cpp
+++ Bank Account Manager/main.cpp
@@ -8,19 +8,10 @@
 template<typename T>
 struct crtp
 {
+protected:
 	constexpr crtp() = default;
 	constexpr auto* underlying() { return static_cast<T*>(this); }
 	constexpr const auto* underlying() const { return static_cast<const T*>(this); }
-};
-
-template<typename T>
-struct IAccountLog : public crtp<T>
-{
-	IAccountLog& logDeposit(float v) noexcept { this->underlying()->logDeposit(v); return *this; }
-	IAccountLog& logTakeOut(float v) noexcept { this->underlying()->logTakeOut(v); return *this; }
-
-protected:
-	IAccountLog() = default;
 };
 
 template<typename T>
@@ -30,111 +21,154 @@ struct IAccount : public crtp<T>
 	IAccount& takeOut(float v) noexcept { this->underlying()->takeOut(v); return *this; }
 	float balance() const noexcept { return this->underlying()->balance(); }
 
+	IAccount& showLogs() noexcept { this->underlying()->showLogs(); return *this; }
+
+	IAccount& setPin(size_t p) noexcept { this->underlying()->setPin(p); return *this; }
+
 protected:
 	IAccount() = default;
 };
 
-template<typename ImplLog>
-class Account : public IAccount<Account<ImplLog>>
+template<typename ImplMon, typename ImplLog, typename ImplSec>
+class Account : public IAccount<Account<ImplMon, ImplLog, ImplSec>>
 {
-	void deposit(float v) noexcept 
-	{ 
-		m_mon += v;
-		m_logger.first.get().logDeposit(v);
-	}
-	void takeOut(float v) noexcept 
-	{ 
-		m_mon -= v; 
-		m_logger.first.get().logTakeOut(v);
-	}
-	auto balance() const noexcept 
-	{
-		return m_mon; 
-	}
-
 public:
 	Account() = default;
 
-	friend struct IAccount<Account>;
-
-private:
-	float m_mon = 0.f;
-
-	std::pair<std::reference_wrapper<IAccountLog<ImplLog>>, ImplLog> m_logger 
-		= std::pair<std::reference_wrapper<IAccountLog<ImplLog>>, ImplLog>(m_logger.second, ImplLog());
-};
-
-template<typename T>
-struct IAccountDB : public crtp<T>
-{
-	auto& account(const std::string_view& str) { return this->underlying()->account(str); }
-
-protected:
-	IAccountDB() = default;
-};
-
-template<typename T>
-class AccountDB : public IAccountDB<AccountDB<T>>
-{
-	auto account(const std::string_view& str) -> IAccount<T>& { return m_accounts[str]; }
-
-public:
-	auto& createAccount(const std::string_view& name)
+	void deposit(float v) noexcept 
+	{ 
+		m_money.transform(v);
+		m_logger.logDeposit(v);
+	}
+	void takeOut(float v) noexcept 
+	{ 
+		m_money.transform(-v);
+		m_logger.logTakeOut(v);
+	}
+	auto balance() const noexcept 
 	{
-		m_accounts.emplace(name, T());
-		return *this;
+		return m_money.balance(); 
+	}
+	void showLogs() const noexcept
+	{
+		m_logger.printAll();
+	}
+	void setPin(size_t p) noexcept
+	{
+		m_sec.pin(p);
+	}
+	bool validatePin(size_t p)
+	{
+		return m_sec.validate(p);
 	}
 
-	friend struct IAccountDB<AccountDB<T>>;
-
 private:
-	std::map<std::string_view, T> m_accounts;
+	ImplMon m_money;
+	ImplLog m_logger;
+	ImplSec m_sec;
 };
 
 template<typename ImplAccDB>
 class ATM
 {
 public:
-	ATM(IAccountDB<ImplAccDB>* db)
+	ATM(ImplAccDB* db)
 		: m_db(db)
 	{
 	}
 
-	auto& access(const std::string_view& account)
+	IAccount<typename ImplAccDB::account_t>* access(const std::string_view& account, uint_fast16_t pin)
 	{
-		return m_db->account(account);
+		auto* acc = &m_db->account(account);
+		if (acc->validatePin(pin))
+			return static_cast<IAccount<typename ImplAccDB::account_t>*>(acc);
+		else
+			return nullptr;
 	}
 
 private:
-	IAccountDB<ImplAccDB>* m_db;
+	ImplAccDB* m_db;
 };
 
-class AccountLogger : public IAccountLog<AccountLogger>
+template<typename T>
+class AccountDB
 {
-	void logDeposit(float v) noexcept 
-	{ 
-		m_logs.emplace_back(std::to_string(v) + " deposited");
-	}
-	void logTakeOut(float v) noexcept 
-	{ 
-		m_logs.emplace_back(std::to_string(v) + " took out");
+public:
+	using account_t = T;
+
+	auto& account(const std::string_view& str) { return m_accounts[str]; }
+
+	auto& createAccount(const std::string_view& name)
+	{
+		m_accounts.emplace(name, T());
+		return *this;
 	}
 
+private:
+	std::map<std::string_view, T> m_accounts;
+};
+
+class AccountLogger
+{
 public:
-	AccountLogger() {}
-	friend struct IAccountLog<AccountLogger>;
+	AccountLogger() = default;
+
+	void logDeposit(float v) noexcept
+	{
+		m_logs.emplace_back(std::to_string(v) + " deposited");
+	}
+	void logTakeOut(float v) noexcept
+	{
+		m_logs.emplace_back(std::to_string(v) + " took out");
+	}
+	void printAll() const noexcept
+	{
+		for (const auto& i : m_logs)
+			std::cout << i << '\n';
+	}
 
 private:
 	std::deque<std::string> m_logs;
 };
 
+class AccountMoney
+{
+public:
+	AccountMoney() = default;
+
+	void transform(float v) noexcept { m_mon += v; }
+	auto balance() const noexcept { return m_mon; }
+
+private:
+	float m_mon = 0.f;
+};
+
+class AccountSecurity
+{
+public:
+	AccountSecurity() = default;
+
+	constexpr void pin(size_t p) noexcept { m_pin = p; }
+	constexpr bool validate(size_t p) const noexcept { return m_pin == p; }
+
+private:
+	uint_fast16_t m_pin = 0000;
+};
+
 int main(int argc, char** argv)
 {
-	AccountDB<Account<AccountLogger>> db;
+	using UserAccount = Account<AccountMoney, AccountLogger, AccountSecurity>;
+
+	AccountDB<UserAccount> db;
 	db.createAccount("ass");
 
 	ATM atm(&db);
-	std::cout << atm.access("ass").deposit(10.f).balance();
+	atm.access("ass", 000)->deposit(100.f)
+		.takeOut(50.f)
+		.deposit(25.f)
+		.setPin(100);
+
+	atm.access("ass", 100)->showLogs();
 
 	return 0;
 }
